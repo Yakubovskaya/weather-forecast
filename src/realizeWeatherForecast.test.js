fcb5cd3c413e57610b3bbca9a0cbd2ccb1eb1373
Marina <maribars93@yandex.ru>
2021-12-04 23:08:06 +0300

    Fix realizeWeatherForecast.test.js

--- src/realizeWeatherForecast.test.js
+++ src/realizeWeatherForecast.test.js
@@ -1,13 +1,13 @@
 import { realizeWeatherForecast } from "./realizeWeatherForecast";
-import { sleep } from "./sleep";
 
 describe("realizeWeatherForecast", () => {
   let el;
   let mockFridge;
   let ipData;
-  let weatherData;
+  let weather;
+  let data;
+  let API_MAP;
   let API_KEY_WEATHER;
-  const delay = 1000;
   const unmockedFetch = global.fetch;
   const unmockedStorageSetItem = global.Storage.prototype.setItem;
   const unmockedStorageGetItem = global.Storage.prototype.getItem;
@@ -15,6 +15,8 @@ describe("realizeWeatherForecast", () => {
 
   beforeAll(() => {
     global.fetch = jest.fn();
+    API_KEY_WEATHER = "b071b1f9cb6a205c491857b865f1bf99";
+    API_MAP = "AIzaSyCVGDC-uRpiX2HiexHVIBHz5k_obk7c1XQ";
     mockFridge = {};
     global.Storage.prototype.setItem = jest.fn((key, value) => {
       mockFridge[key] = value;
@@ -32,15 +34,26 @@ describe("realizeWeatherForecast", () => {
     mockFridge = {};
     el = document.createElement("div");
     document.body.append(el);
+
+    global.fetch
+      .mockResolvedValueOnce({ json: () => Promise.resolve(ipData) })
+      // eslint-disable-next-line max-len
+      .mockResolvedValueOnce({ json: () => Promise.resolve(weather) });
+  });
+
+  afterEach(() => {
+    document.querySelector("html").innerHTML = null;
+  });
+
+  it("show weather and map in User city", async () => {
     const lon = 30.2618;
     const lat = 59.8983;
     ipData = { latitude: lat, longitude: lon };
-    API_KEY_WEATHER = "b071b1f9cb6a205c491857b865f1bf99";
     const cityName = "Saint Petersburg";
     const description = "light rain";
     const icon = "10n";
     const temp = 278.47;
-    weatherData = {
+    weather = {
       weather: [
         {
           description,
@@ -53,19 +66,7 @@ describe("realizeWeatherForecast", () => {
       name: cityName,
     };
 
-    global.fetch
-      .mockResolvedValueOnce({ json: () => Promise.resolve(ipData) })
-      // eslint-disable-next-line max-len
-      .mockResolvedValueOnce({ json: () => Promise.resolve(weatherData) });
-  });
-
-  afterEach(() => {
-    document.querySelector("html").innerHTML = null;
-  });
-
-  it("show weather and map in User city", async () => {
-    realizeWeatherForecast(el);
-    await sleep(delay);
+    await realizeWeatherForecast(el);
     expect(global.fetch).toHaveBeenNthCalledWith(1, "https://ipapi.co/json/", {
       method: "GET",
     });
@@ -76,19 +77,17 @@ describe("realizeWeatherForecast", () => {
         method: "GET",
       }
     );
-    const weatherCard = document.querySelector(".weather-wrapper");
-    const cityEl = weatherCard.querySelector(".city");
-    const tempEl = weatherCard.querySelector(".temp");
-    const descrEl = weatherCard.querySelector(".descr");
-    const iconEl = weatherCard.querySelector(".icon");
-    const mapEl = weatherCard.querySelector(".map");
-    const API_MAP = "AIzaSyCVGDC-uRpiX2HiexHVIBHz5k_obk7c1XQ";
+    const cityEl = el.querySelector(".city");
+    const tempEl = el.querySelector(".temp");
+    const descrEl = el.querySelector(".descr");
+    const iconEl = el.querySelector(".icon");
+    const mapEl = el.querySelector(".map");
 
-    const expCityEl = `${weatherData.name}`;
-    const expTempEl = `${Math.round(weatherData.main.temp - 273)}°`;
-    const expDescrEl = `${weatherData.weather[0].description}`;
-    const expIconEl = `<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png">`;
-    const expMapEl = `<img src="https://maps.googleapis.com/maps/api/staticmap?center=${weatherData.name}&amp;zoom=9&amp;size=300x300&amp;&amp;markers=size:mid%7Ccolor:0xFFFF00%7C${weatherData.name}&amp;key=${API_MAP}">`;
+    const expCityEl = `${weather.name}`;
+    const expTempEl = `${Math.round(weather.main.temp - 273)}°`;
+    const expDescrEl = `${weather.weather[0].description}`;
+    const expIconEl = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png">`;
+    const expMapEl = `<img src="https://maps.googleapis.com/maps/api/staticmap?center=${weather.name}&amp;zoom=9&amp;size=300x300&amp;&amp;markers=size:mid%7Ccolor:0xFFFF00%7C${weather.name}&amp;key=${API_MAP}">`;
     expect(cityEl.innerHTML).toStrictEqual(expCityEl);
     expect(tempEl.innerHTML).toStrictEqual(expTempEl);
     expect(descrEl.innerHTML).toStrictEqual(expDescrEl);
@@ -97,13 +96,65 @@ describe("realizeWeatherForecast", () => {
   });
 
   it("shows LocalStorage items when app has started", async () => {
-    const itemsArray = ["Выборг", "Москва", "Тверь"];
+    const itemsArray = '["Выборг", "Москва", "Тверь"]';
     mockFridge[storageKey] = itemsArray;
-    realizeWeatherForecast(el);
-    await sleep(delay);
+    await realizeWeatherForecast(el);
     const ulEl = document.querySelector("ul");
     expect(ulEl.innerHTML).toStrictEqual(
       "<li>Выборг</li><li>Москва</li><li>Тверь</li>"
     );
+  });
+  it("shows weather and map in the city entered in input element and push city in LocalStrg", async () => {
+    const city = "Moscow";
+    const description = "clear sky";
+    const icon = "01d";
+    const temp = 282.55;
+    data = {
+      weather: [
+        {
+          description,
+          icon,
+        },
+      ],
+      main: {
+        temp,
+      },
+      name: city,
+    };
+
+    global.fetch.mockResolvedValueOnce({ json: () => Promise.resolve(data) });
+
+    await realizeWeatherForecast(el);
+
+    const form = el.querySelector("form");
+    const input = el.querySelector("input");
+    input.value = city;
+
+    await form.dispatchEvent(new Event("submit"));
+
+    const cityEl = el.querySelector(".city");
+    const tempEl = el.querySelector(".temp");
+    const descrEl = el.querySelector(".descr");
+    const iconEl = el.querySelector(".icon");
+    const mapEl = el.querySelector(".map");
+
+    const expCityEl = `${data.name}`;
+    const expTempEl = `${Math.round(data.main.temp - 273)}°`;
+    const expDescrEl = `${data.weather[0].description}`;
+    const expIconEl = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+    const expMapEl = `<img src="https://maps.googleapis.com/maps/api/staticmap?center=${data.name}&amp;zoom=9&amp;size=300x300&amp;&amp;markers=size:mid%7Ccolor:0xFFFF00%7C${data.name}&amp;key=${API_MAP}">`;
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY_WEATHER}`,
+      {
+        method: "GET",
+      }
+    );
+
+    expect(cityEl.innerHTML).toStrictEqual(expCityEl);
+    expect(tempEl.innerHTML).toStrictEqual(expTempEl);
+    expect(descrEl.innerHTML).toStrictEqual(expDescrEl);
+    expect(iconEl.innerHTML).toStrictEqual(expIconEl);
+    expect(mapEl.innerHTML).toStrictEqual(expMapEl);
   });
 });
